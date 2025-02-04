ea07976689293280031057aeb84a25afa534648b
Viktor Oreshkin <imselfish@stek29.rocks>
2025-02-04 04:32:02 +0300

    fix typo in repo name

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/zoo-id-gen/',
+  base: '/zoo-ids-gen/',
   plugins: [
     tsconfigPaths(),
     react(),
